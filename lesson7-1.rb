c110046725b2798e4aab7b2b5db1858369545401
EC2 Default User <ec2-user@ip-172-31-75-253.ec2.internal>
2021-07-21 08:55:08 +0000

    [Update]確認問題１

--- lesson7-1.rb
+++ lesson7-1.rb
@@ -1,8 +1,9 @@
 puts "計算を始めます。"
 puts "2つの値を入力して下さい。"
-input_key = gets.to_i
-puts "#{input_key}"
-puts "#{input_key}"
+
+a = gets.to_i
+b = gets.to_i
+
 puts "計算結果を出力します。"
-puts "a*b="
+puts "a*b=#{a * b}"
 puts "計算を終了します。"
