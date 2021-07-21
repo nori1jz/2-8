736f1ff3462c323923d0959b2bb5876d5b1da3af
EC2 Default User <ec2-user@ip-172-31-75-253.ec2.internal>
2021-07-21 12:18:13 +0000

    [Uplaud]確認問題２

--- lesson7-2.rb
+++ lesson7-2.rb
@@ -1,14 +1,18 @@
 puts "計算を始めます。"
 puts "何回繰り返しますか。"
 
-n = gets .to_i
+input = gets.to_i
 
-while #{n-1}
-puts "1回目の計算"
-puts "２つの値を入力してください"
+i = 1
+while i <= input do
+ puts "#{i}回目の計算"
+ puts "２つの値を入力してください"
 
 a = gets.to_i
 b = gets.to_i
+
+puts "a=#{a}"
+puts "b=#{b}"
 
 puts "計算結果を出力します。"
 puts "a+b=#{a + b }"
@@ -16,7 +20,8 @@ puts "a-b=#{a - b }"
 puts "a*b=#{a * b }"
 puts "a/b=#{a / b }"
 
+i += 1
 
-
-  
 end
+
+puts "計算を終了します"
