64958ed213a4630e6316e31f248cc873e8116fa8
EC2 Default User <ec2-user@ip-172-31-75-253.ec2.internal>
2021-07-21 12:05:24 +0000

    [finish]確認問題

---
+++ lesson7-2.rb
@@ -0,0 +1,22 @@
+puts "計算を始めます。"
+puts "何回繰り返しますか。"
+
+n = gets .to_i
+
+while #{n-1}
+puts "1回目の計算"
+puts "２つの値を入力してください"
+
+a = gets.to_i
+b = gets.to_i
+
+puts "計算結果を出力します。"
+puts "a+b=#{a + b }"
+puts "a-b=#{a - b }"
+puts "a*b=#{a * b }"
+puts "a/b=#{a / b }"
+
+
+
+  
+end
