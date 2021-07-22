9acfae6028a42f43b94fe2bd2db93d339b0f91f0
EC2 Default User <ec2-user@ip-172-31-75-253.ec2.internal>
2021-07-22 13:25:49 +0000

    [Update]FizzBuzzプログラム

--- fizz_buzz.rb
+++ fizz_buzz.rb
@@ -1,9 +1,14 @@
 def fizz_buzz(number)
-  return "fizz" if num.3
-  return "buzz" if num.5
-  return "FizzBuzz" if num.15
-  
-end 
+  if number % 15 == 0
+    "FizzBuzz"
+  elsif number % 3 == 0
+    "Fizz"
+  elsif number % 5 == 0
+    "Buzz"
+  else 
+    number.to_s
+  end 
+end  
 
 puts "数字を入力してください。"
 
